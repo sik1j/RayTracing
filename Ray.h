ce68b1eb5510d65009ed19d1b1a2a8841cbfe35f
sik1j <karkisikij2@gmail.com>
2023-06-23 10:26:56 -0700

    add prefix m to member fields, and a semicolon The semicolon at the end is needed else a bug occurs

--- Ray.h
+++ Ray.h
@@ -7,19 +7,19 @@
 // where O is the origin, D is the direction, and t is a scalar.
 class Ray {
 private:
-    Point3 origin;
-    Vec3 direction;
+    Point3 mOrigin;
+    Vec3 mDirection;
 public:
     Ray() {}
     Ray(const Point3 &origin, const Vec3 &direction)
-        : origin(origin), direction(direction) {}
+        : mOrigin(origin), mDirection(direction) {}
 
-    Point3 origin() const { return origin; }
-    Vec3 direction() const { return direction; }
+    Point3 origin() const { return mOrigin; }
+    Vec3 direction() const { return mDirection; }
 
     Point3 at(double t) {
-        return origin + t * direction;
+        return mOrigin + t * mDirection;
     }
-}
+};
 
 #endif
