dd2bf2fd74a0f9ada6d92375187b4854e845a8bf
sik1j <karkisikij2@gmail.com>
2023-06-22 18:17:19 -0700

    Add a progress indicator to image creation

--- main.cc
+++ main.cc
@@ -10,9 +10,10 @@ int main() {
 
     // green decreases as row's increase
     for (int row = image_height-1; row >= 0; row--) {
+        std::cerr << "\rScanline remaining: " << row << ' ' << std::flush;
         // red increases as col's increase
         for (int col = 0; col < image_width; col++) {
-            // division maps: [0, image_height-1] -> [0, 1]
+            // linearly maps: [0, image_height-1] -> [0, 1]
             double red = (double)row/(image_height - 1); 
             double green = (double)col/(image_width - 1);
             double blue = 0.25;
@@ -24,4 +25,6 @@ int main() {
             std::cout << ired << ' ' << igreen << ' ' << iblue << '\n';
         }
     }
+
+    std::cerr << "\nDone\n";
 }
