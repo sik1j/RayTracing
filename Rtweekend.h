b41b6e779bf530b756d60893387c1e2fd2b12c1e
sik1j <karkisikij2@gmail.com>
2023-06-24 18:49:00 -0700

    Add helper functions.
    Added a random number generator, and a clamp function

--- Rtweekend.h
+++ Rtweekend.h
@@ -1,6 +1,7 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include <cstdlib>
 #include <cmath>
 #include <limits>
 #include <memory>
@@ -12,8 +13,24 @@ const double pi = 3.1415926535897932385;
 
 // Utility Functions
 
-inline double degrees_to_radians(double degrees) {
+double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+double random_double() {
+    // returns double in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+double random_double(double min, double max) {
+    // returns a double in [min, max)
+    return random_double()*(max-min) + min;
+}
+
+double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
