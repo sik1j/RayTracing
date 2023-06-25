84374bf9f586da0f58332091101348eaa2a17f43
sik1j <karkisikij2@gmail.com>
2023-06-24 17:16:18 -0700

    create general header
    Contains common constants, headers, and utility functions

---
+++ Rtweekend.h
@@ -0,0 +1,24 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+
+#include "Ray.h"
+#include "Vec3.h"
+
+#endif
