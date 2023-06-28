b5dda7762e0fae8ded71866a8a254130c2ef7829
sik1j <karkisikij2@gmail.com>
2023-06-28 14:55:12 -0700

    Create Lambertian (diffuse) material class
    Represents a material that scatters light diffusely

--- Material.h
+++ Material.h
@@ -10,7 +10,30 @@ struct HitRecord;
 class Material {
 public:
   virtual bool scatter(const Ray &ray_in, const HitRecord &record,
-                       Color &attenuation, Ray &scattered) = 0;
+                       Color &attenuation, Ray &scattered) const = 0;
+};
+
+class Lambertian : public Material {
+private:
+  // the amount of light reflected by surface
+  Color albedo;
+
+public:
+  Lambertian(const Color &albedo) : albedo(albedo){};
+
+  bool scatter(const Ray &ray_in, const HitRecord &record, Color &attenuation,
+               Ray &scattered) const override {
+    auto scatter_direction = record.normal + random_unit_vector();
+    scattered = ray(record.p, scatter_direction);
+
+    // if random_unit_vector happens to be opposite to record.normal
+    if (scatter_direction.near_zero()) {
+      scatter_direction = record.normal;
+    }
+
+    attenuation = albedo;
+    return true;
+  }
 };
 
 #endif
