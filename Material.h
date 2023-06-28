b844e95296fe0d12bf0ffc26a0076a8054bfb06d
sik1j <karkisikij2@gmail.com>
2023-06-28 16:58:56 -0700

    Add Metal material class

--- Material.h
+++ Material.h
@@ -7,13 +7,16 @@
 
 struct HitRecord;
 
-class Material {
+class Material
+{
 public:
+  /// @return false if ray was absorbed
   virtual bool scatter(const Ray &ray_in, const HitRecord &record,
                        Color &attenuation, Ray &scattered) const = 0;
 };
 
-class Lambertian : public Material {
+class Lambertian : public Material
+{
 private:
   // the amount of light reflected by surface
   Color albedo;
@@ -22,12 +25,14 @@ public:
   Lambertian(const Color &albedo) : albedo(albedo){};
 
   bool scatter(const Ray &ray_in, const HitRecord &record, Color &attenuation,
-               Ray &scattered) const override {
+               Ray &scattered) const override
+  {
     auto scatter_direction = record.normal + random_unit_vector();
     scattered = Ray(record.point, scatter_direction);
 
     // if random_unit_vector happens to be opposite to record.normal
-    if (scatter_direction.near_zero()) {
+    if (scatter_direction.near_zero())
+    {
       scatter_direction = record.normal;
     }
 
@@ -35,5 +40,23 @@ public:
     return true;
   }
 };
+
+class Metal : public Material
+{
+private:
+  Color albedo;
+
+public:
+  Metal(const Color &albedo) : albedo(albedo){};
+
+  bool scatter(const Ray &ray_in, const HitRecord &record, Color &attenuation,
+               Ray &scattered) const override
+  {
+    Vec3 reflected = reflect(unit_vector(ray_in.direction()), record.normal);
+    scattered = Ray(record.point, reflected);
+    attenuation = albedo;
+    return (dot(scattered.direction(), record.normal) > 0);
+  }
+}
 
 #endif
