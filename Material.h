cc5a4d9c2b7f6e9dee076eb98fc314f72028e867
sik1j <karkisikij2@gmail.com>
2023-06-28 20:14:18 -0700

    implement reflectance in Dielectric material class

--- Material.h
+++ Material.h
@@ -68,6 +68,14 @@ class Dielectric : public Material
 private:
   double refraction_index;
 
+  static double reflectance(double cosine, double ref_idx)
+  {
+    // Schlick's approximation for reflectance
+    auto r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+  }
+
 public:
   Dielectric(double refraction_index) : refraction_index(refraction_index){};
 
@@ -95,7 +103,7 @@ public:
 
     bool cannot_refract = refraction_ratio * sin_theta > 1.0;
     Vec3 direction;
-    if (cannot_refract)
+    if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
     {
       direction = reflect(unit_direction, record.normal);
     }
