f642a89eec860a61bd4123d5d5118c566dc5d233
sik1j <karkisikij2@gmail.com>
2023-06-28 17:28:39 -0700

    Create a scene with reflective metallic balls

--- main.cc
+++ main.cc
@@ -9,6 +9,7 @@
 #include "Vec3.h"
 #include "Render.h"
 #include "Material.h"
+#include "HitRecord.h"
 
 Color ray_color(const Ray &ray, const Hittable &world, int depth) {
     // recursion limit to proctect the stack from blowing up
@@ -51,8 +52,16 @@ int main() {
 
     // World to render
     HittableList world;
-    world.add(std::make_shared<Sphere>(Point3(0,0, -1), 0.5));
-    world.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100));
+
+    auto material_ground = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<Lambertian>(Color(0.7, 0.3, 0.3));
+    auto material_left   = std::make_shared<Metal>(Color(0.8, 0.8, 0.8));
+    auto material_right  = std::make_shared<Metal>(Color(0.8, 0.6, 0.2));
+
+    world.add(std::make_shared<Sphere>(Point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(std::make_shared<Sphere>(Point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(std::make_shared<Sphere>(Point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(std::make_shared<Sphere>(Point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
     auto camera = Camera();
